85324cb4e7cac2cd402e9f685044dfc4471ca045
joferava <jfelipe221095@gmail.com>
2021-02-27 21:03:27 -0600

    Initial Commit

---
+++ SwiftUICalculadoraCenfotec/SwiftUICalculadoraCenfotecApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalculadoraCenfotecApp.swift
+//  SwiftUICalculadoraCenfotec
+//
+//  Created by Felipe Ramirez Vargas on 27/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalculadoraCenfotecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
